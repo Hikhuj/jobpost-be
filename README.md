9e975deedc6b85d31945bedcadc34a6ccefd8412
Roger Ulate <roger.p.designe357@gmail.com>
2024-09-22 19:11:44 -0600

    Varios

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 
 - Required to use Python v.3
 - Create virtual ENV with venv (https://flask.palletsprojects.com/en/3.0.x/installation/)
-    [code] python3 -m venv <directory>
-    [code] python3 -m venv venv (mostly common name)
+    [code]python3 -m venv <directory>
+    [code]python3 -m venv venv (mostly common name)
 - To start the Python Venv
-    [code] source myvenv/bin/activate
+    [code]source myvenv/bin/activate
 - To deactivate, write "deactivate" on the project because any code you execute about python, will be executed outside too.
-    [code] deactivate
+    [code]deactivate
 - Install the requirements with the command
-    [code] pip install -r requirements.txt
+    [code]pip install -r requirements.txt
+- To run Flask App do
+    [code]flask --app app run[/code]
+- Run the app on debugg mode
+    [code]flask --app app run --debug[/code]
 
 Para activar los ambientes virtuales de Python
 
@@ -19,6 +23,7 @@ Activate pythong environment.
 
 pip3 install Flask
 pip3 install pillow imgkit
+pip3 install flask-cors
 
 
 To run application
--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+import os
+
 import imgkit
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
+
+# allow request from the FE 
 CORS(app, resources={r"/data_to_image": {"origins": "http://localhost:3000"}})
 
 @app.route("/")
@@ -12,131 +16,7 @@ def home():
 
 @app.route("/data_to_image", methods=["POST"])
 def data_to_image():
-
-    test_variable_1 = "Roger Ulate"
-    test_variable_2 = "Priscilla Ulate"
-
-    HTML = f"""
-    <style media="screen,print">
-        #g-Positions-box ,
-        #g-Positions-box .g-artboard {
-            margin:0 auto;
-        }
-        #g-Positions-box p {
-            margin:0;
-        }
-        #g-Positions-box .g-aiAbs {
-            position:absolute;
-        }
-        #g-Positions-box .g-aiImg {
-            position:absolute;
-            top:0;
-            display:block;
-            width:100% !important;
-        }
-        #g-Positions-box .g-aiSymbol {
-            position: absolute;
-            box-sizing: border-box;
-        }
-        #g-Positions-box .g-aiPointText p { white-space: nowrap; }
-        #g-Positions-Mesa_de_trabajo_1 {
-            position:relative;
-            overflow:hidden;
-        }
-        #g-Positions-Mesa_de_trabajo_1 p {
-            font-weight:regular;
-            line-height:24px;
-            height:auto;
-            opacity:1;
-            letter-spacing:0em;
-            font-size:20px;
-            text-align:left;
-            color:rgb(0,0,0);
-            text-transform:none;
-            padding-bottom:0;
-            padding-top:0;
-            mix-blend-mode:normal;
-            font-style:normal;
-            position:static;
-        }
-        #g-Positions-Mesa_de_trabajo_1 .g-pstyle0 {
-            font-weight:500;
-            line-height:75px;
-            height:75px;
-            letter-spacing:-0.05em;
-            font-size:89px;
-        }
-        #g-Positions-Mesa_de_trabajo_1 .g-pstyle1 {
-            font-weight:500;
-            line-height:75px;
-            height:75px;
-            letter-spacing:-0.05em;
-            font-size:89px;
-            color:rgb(255,255,255);
-        }
-        #g-Positions-Mesa_de_trabajo_1 .g-pstyle2 {
-            font-weight:500;
-            line-height:48px;
-            height:48px;
-            font-size:40px;
-            color:rgb(247,240,240);
-        }
-        #g-Positions-Mesa_de_trabajo_1 .g-pstyle3 {
-            font-weight:700;
-            height:24px;
-        }
-        #g-Positions-Mesa_de_trabajo_1 .g-pstyle4 {
-            font-weight:500;
-            height:24px;
-        }
-        #g-Positions-Mesa_de_trabajo_1 .g-cstyle0 {
-            color:rgb(0,0,0);
-        }
-        #g-Positions-Mesa_de_trabajo_1 .g-cstyle1 {
-            font-weight:700;
-        }
-    </style>
-    <div id="g-Positions-box" class="ai2html">
-        <!-- Artboard: Mesa_de_trabajo_1 -->
-        <div id="g-Positions-Mesa_de_trabajo_1" class="g-artboard" style="max-width: 1080px;max-height: 1080px" data-aspect-ratio="1" data-min-width="0">
-    <div style="padding: 0 0 100% 0;"></div>
-            <img id="g-Positions-Mesa_de_trabajo_1-img" class="g-Positions-Mesa_de_trabajo_1-img g-aiImg" alt="" src="Positions-Mesa_de_trabajo_1.jpg"/>
-            <div id="g-ai0-1" class="g-Propuesta_2 g-aiAbs g-aiPointText" style="top:8.7412%;margin-top:-81.4px;left:4.0679%;width:504px;">
-                <p class="g-pstyle0">WE ARE</p>
-                <p class="g-pstyle1">LOOKING<span class="g-cstyle0"> FOR</span></p>
-            </div>
-            <div id="g-ai0-2" class="g-Propuesta_2 g-aiAbs g-aiPointText" style="top:21.7414%;margin-top:-26.8px;left:5.0612%;width:659px;">
-                <p class="g-pstyle2">Sr. Full Stack Engineer (Angular/.Net)</p>
-            </div>
-            <div id="g-ai0-3" class="g-Propuesta_2 g-aiAbs g-aiPointText" style="top:48.7644%;margin-top:-181.7px;left:32.8452%;width:730px;">
-                <p class="g-pstyle3">Requirements</p>
-                <p>&nbsp;</p>
-                <p class="g-pstyle4">{test_variable_1}</p>
-                <p class="g-pstyle4">{test_variable_2}</p>
-                <p>&nbsp;</p>
-                <p class="g-pstyle3">Preferred Skills and Experience</p>
-                <p>&nbsp;</p>
-                <p class="g-pstyle4"><span class="g-cstyle1">   &bull; </span>Experience with Python</p>
-                <p class="g-pstyle4">   &bull; Experience with cloud platforms such as AWS and Azure</p>
-                <p class="g-pstyle4">   &bull; Familiarity with debugging front-end rendering & performance issues </p>
-                <p class="g-pstyle4">      using browser development tools</p>
-                <p class="g-pstyle4">   &bull; Understanding of how to optimize a component&rsquo;s UI/UX performance</p>
-            </div>
-        </div>
-    </div>
-    """
-
-    # Options for wkhtmltoimage
-    options = {
-        'format': 'png',
-        'width': 500,
-        'height': 500,
-        'encoding': "UTF-8",
-        'quality': '100'
-    }
-
-    # Convert HTML to image
-    imgkit.from_string(HTML, "/output_image.png", options=options)
+    return "hello"
 
 if __name__ == "__main__":
     app.run(debug=True)
